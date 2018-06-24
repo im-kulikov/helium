eb3f26d88c6fd3395f8e51b9393a3a7216e89776
Evgeniy Kulikov <im@kulikov.im>
2018-06-25 00:37:21 +0300

    Bump new version 0.4.1 - add capture error

--- web/engine.go
+++ web/engine.go
@@ -1,9 +1,68 @@
 package web
 
 import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"net/http"
+
+	"github.com/im-kulikov/helium/logger"
 	"github.com/im-kulikov/helium/validate"
 	"github.com/labstack/echo"
 )
+
+type (
+	errorResponse struct {
+		Error  string   `json:"error"`
+		Stack  []string `json:"stack"`
+		Result []string `json:"result"`
+	}
+
+	CustomError interface {
+		FormatResponse(ctx echo.Context)
+	}
+)
+
+func captureError(err error, ctx echo.Context) {
+	var (
+		message string
+		code    = http.StatusBadRequest
+		result  = make([]string, 0)
+		trace   = make([]string, 0)
+	)
+
+	switch custom := err.(type) {
+	case CustomError:
+		custom.FormatResponse(ctx)
+		return
+	case *json.UnmarshalTypeError:
+		message = fmt.Sprintf("JSON parse error: expected=%v, got=%v, offset=%v", custom.Type, custom.Value, custom.Offset)
+	case *json.SyntaxError:
+		message = fmt.Sprintf("JSON parse error: offset=%v, error=%v", custom.Offset, custom.Error())
+	case *xml.UnsupportedTypeError:
+		message = fmt.Sprintf("XML parse error: type=%v, error=%v", custom.Type, custom.Error())
+	case *xml.SyntaxError:
+		message = fmt.Sprintf("XML parse error: line=%v, error=%v", custom.Line, custom.Error())
+	case *echo.HTTPError:
+		code = custom.Code
+		message = custom.Message.(string)
+	default:
+		message = http.StatusText(code)
+	}
+
+	// Capture errors:
+	if code >= http.StatusInternalServerError {
+		logger.G().Errorw("Request error", "error", err)
+	}
+
+	if errJSON := ctx.JSON(code, errorResponse{
+		Error:  message,
+		Stack:  trace,
+		Result: result,
+	}); errJSON != nil {
+		logger.G().Errorw("Capture error", "error", err)
+	}
+}
 
 func NewEngine(l echo.Logger, b echo.Binder, v validate.Validator) *echo.Echo {
 	e := echo.New()
@@ -13,6 +72,7 @@ func NewEngine(l echo.Logger, b echo.Binder, v validate.Validator) *echo.Echo {
 	e.Logger = l
 	e.Validator = v
 	e.Binder = b
+	e.HTTPErrorHandler = captureError
 
 	return e
 }
