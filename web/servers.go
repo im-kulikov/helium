99c6a8016b73d401a852d3d2f0c6a94807a47950
Evgeniy Kulikov <im@kulikov.im>
2018-06-25 01:29:36 +0300

    Bump new version 0.4.3 - setup mserv logger

--- web/servers.go
+++ web/servers.go
@@ -5,10 +5,12 @@ import (
 
 	"github.com/chapsuk/mserv"
 	"github.com/im-kulikov/helium/settings"
+	"github.com/labstack/echo"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func NewServers(handler http.Handler) mserv.Server {
+func NewServers(handler http.Handler, log echo.Logger) mserv.Server {
+	mserv.SetLogger(log)
 	return mserv.New(
 		mserv.NewHTTPServer(settings.HTTPServer("pprof", http.DefaultServeMux)),
 		mserv.NewHTTPServer(settings.HTTPServer("metrics", promhttp.Handler())),
