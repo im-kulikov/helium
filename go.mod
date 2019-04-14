67e599a84be7a8244410db6ab626cc0f12a4ad56
Evgeniy Kulikov <im@kulikov.im>
2019-04-15 00:26:53 +0300

    Prepare to v0.11.5 release, from now
    - phasing out go-convey

--- go.mod
+++ go.mod
@@ -22,7 +22,7 @@ require (
 	github.com/smartystreets/goconvey v0.0.0-20190222223459-a17d461953aa
 	github.com/spf13/afero v1.2.1 // indirect
 	github.com/spf13/viper v1.3.1
-	github.com/stretchr/testify v1.3.0 // indirect
+	github.com/stretchr/testify v1.3.0
 	go.uber.org/atomic v1.3.2 // indirect
 	go.uber.org/dig v1.7.0
 	go.uber.org/multierr v1.1.0 // indirect
--- grace/grace_test.go
+++ grace/grace_test.go
@@ -1,38 +1,37 @@
 package grace
 
 import (
-	"errors"
 	"fmt"
 	"syscall"
 	"testing"
 	"time"
 
-	. "github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
 )
 
 func TestGrace(t *testing.T) {
-	Convey("check grace context", t, func(c C) {
-		var (
-			log = zap.L()
-			ctx = NewGracefulContext(log)
-		)
+	var (
+		log = zap.L()
+		ctx = NewGracefulContext(log)
+	)
 
-		// waiting to run the goroutine and channel of signals
-		<-time.Tick(100 * time.Millisecond)
+	// waiting to run the goroutine and channel of signals
+	<-time.Tick(100 * time.Millisecond)
 
-		for _, sig := range []syscall.Signal{syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP} {
-			c.Convey(fmt.Sprintf("should cancel context on %s signal", sig), func(c C) {
-				err := syscall.Kill(syscall.Getpid(), sig)
-				c.So(err, ShouldBeNil)
+	for _, sig := range []syscall.Signal{syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP} {
+		t.Run(fmt.Sprintf("should cancel context on %s signal", sig), func(t *testing.T) {
+			is := assert.New(t)
 
-				select {
-				case <-ctx.Done():
-					c.So(true, ShouldBeTrue)
-				case <-time.Tick(time.Second):
-					c.So(errors.New("no signal"), ShouldBeNil)
-				}
-			})
-		}
-	})
+			err := syscall.Kill(syscall.Getpid(), sig)
+			is.NoError(err)
+
+			select {
+			case <-ctx.Done():
+				return
+			case <-time.Tick(time.Second):
+				t.Fatal("no signal")
+			}
+		})
+	}
 }
