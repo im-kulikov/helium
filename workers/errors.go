24f16f2ea07da4128aad79fb487e83f6a546335a
Evgeniy Kulikov <im@kulikov.im>
2019-04-25 21:04:08 +0300

    fix golint warnings

--- workers/errors.go
+++ workers/errors.go
@@ -1,12 +1,17 @@
 package workers
 
+// Error is constant error
 type Error string
 
 const (
-	ErrMissingKey     = Error("missing worker key")
+	// ErrMissingKey when config key for worker is missing
+	ErrMissingKey = Error("missing worker key")
+
+	// ErrRedisClientNil when redis client not provided
 	ErrRedisClientNil = Error("gotten nil redis client for exclusive worker")
 )
 
+// Error converts constant error from string
 func (e Error) Error() string {
 	return string(e)
 }
