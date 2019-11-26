d13073dd044150dcab48e2595fa4e68b84eb7f54
Evgeniy Kulikov <im@kulikov.im>
2019-11-27 02:46:12 +0300

    web: prepare gRPC, http and listener services
    Signed-off-by: Evgeniy Kulikov <im@kulikov.im>

---
+++ web/http.go
@@ -0,0 +1,144 @@
+package web
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/im-kulikov/helium/internal"
+)
+
+type (
+	httpService struct {
+		skipErrors      bool
+		address         string
+		network         string
+		server          *http.Server
+		shutdownTimeout time.Duration
+	}
+
+	// HTTPOption interface that allows
+	// to change default http-service options.
+	HTTPOption func(s *httpService)
+)
+
+const (
+	// ErrEmptyHTTPServer is raised when called New
+	// or httpService methods with empty http.Server.
+	ErrEmptyHTTPServer = internal.Error("empty http server")
+
+	// ErrEmptyHTTPAddress is raised when passed empty address to NewHTTPService.
+	ErrEmptyHTTPAddress = internal.Error("empty http address")
+)
+
+// HTTPShutdownTimeout changes default shutdown timeout.
+func HTTPShutdownTimeout(v time.Duration) HTTPOption {
+	return func(s *httpService) {
+		s.shutdownTimeout = v
+	}
+}
+
+// HTTPListenNetwork allows to change default (tcp)
+// network for net.Listener.
+func HTTPListenNetwork(network string) HTTPOption {
+	return func(s *httpService) {
+		s.network = network
+	}
+}
+
+// HTTPListenAddress allows to change network for net.Listener.
+// By default it takes address from http.Server.
+func HTTPListenAddress(address string) HTTPOption {
+	return func(s *httpService) {
+		s.address = address
+	}
+}
+
+// HTTPSkipErrors allows to skip any errors
+func HTTPSkipErrors() HTTPOption {
+	return func(s *httpService) {
+		s.skipErrors = true
+	}
+}
+
+// NewHTTPService creates Service from http.Server and HTTPOption's.
+func NewHTTPService(serve *http.Server, opts ...HTTPOption) (Service, error) {
+	if serve == nil {
+		return nil, ErrEmptyHTTPServer
+	}
+
+	s := &httpService{
+		skipErrors:      false,
+		server:          serve,
+		network:         "tcp",
+		address:         serve.Addr,
+		shutdownTimeout: time.Second * 30,
+	}
+
+	for i := range opts {
+		opts[i](s)
+	}
+
+	if s.address == "" {
+		return nil, ErrEmptyHTTPAddress
+	}
+
+	return s, nil
+}
+
+// Start runs http.Server and returns error
+// if something went wrong.
+func (s *httpService) Start() error {
+	var (
+		err error
+		lis net.Listener
+	)
+
+	if s.server == nil {
+		return s.catch(ErrEmptyHTTPServer)
+	} else if lis, err = net.Listen(s.network, s.address); err != nil {
+		return s.catch(err)
+	}
+
+	go func() {
+		var err error
+
+		switch {
+		case s.server.TLSConfig == nil:
+			err = s.server.Serve(lis)
+		default:
+			// provide cert and key from TLSConfig
+			err = s.server.ServeTLS(lis, "", "")
+		}
+
+		// ignores known error
+		if err = s.catch(err); err != nil {
+			panic(fmt.Sprintf("could not start http.Server: %v", err))
+		}
+	}()
+
+	return nil
+}
+
+// Stop tries to stop http.Server and returns error
+// if something went wrong.
+func (s *httpService) Stop() error {
+	if s.server == nil {
+		return s.catch(ErrEmptyHTTPServer)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), s.shutdownTimeout)
+	defer cancel()
+
+	return s.catch(s.server.Shutdown(ctx))
+}
+
+func (s *httpService) catch(err error) error {
+	if s.skipErrors || err == http.ErrServerClosed {
+		return nil
+	}
+
+	return err
+}
---
+++ web/listener.go
@@ -0,0 +1,128 @@
+package web
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/im-kulikov/helium/internal"
+)
+
+type (
+	// Listener service interface.
+	Listener interface {
+		ListenAndServe() error
+		Shutdown(context.Context) error
+	}
+
+	listener struct {
+		skipErrors      bool
+		ignoreErrors    []error
+		server          Listener
+		shutdownTimeout time.Duration
+	}
+
+	// ListenerOption options that allows to change
+	// default settings for Listener service.
+	ListenerOption func(l *listener)
+)
+
+// ErrEmptyListener is raised when an empty Listener
+// used in the NewListener function or Listener methods.
+const ErrEmptyListener = internal.Error("empty listener")
+
+// ListenerSkipErrors allows for ignoring all raised errors.
+func ListenerSkipErrors() ListenerOption {
+	return func(l *listener) {
+		l.skipErrors = true
+	}
+}
+
+// ListenerIgnoreError allows for ignoring all passed errors.
+func ListenerIgnoreError(errors ...error) ListenerOption {
+	return func(l *listener) {
+		l.ignoreErrors = errors
+	}
+}
+
+// ListenerShutdownTimeout allows changing the default shutdown timeout.
+func ListenerShutdownTimeout(v time.Duration) ListenerOption {
+	return func(l *listener) {
+		l.shutdownTimeout = v
+	}
+}
+
+// NewListener creates new Listener service and applies passed options to it.
+func NewListener(lis Listener, opts ...ListenerOption) (Service, error) {
+	if lis == nil {
+		return nil, ErrEmptyListener
+	}
+
+	s := &listener{
+		server:          lis,
+		skipErrors:      false,
+		shutdownTimeout: time.Second * 30,
+	}
+
+	for i := range opts {
+		opts[i](s)
+	}
+
+	return s, nil
+}
+
+// Start tries to start the Listener and returns an error
+// if the Listener is empty. If something went wrong and
+// errors not ignored should panic.
+func (l *listener) Start() error {
+	if l.server == nil {
+		return l.catch(ErrEmptyListener)
+	}
+
+	go func() {
+		if err := l.catch(l.server.ListenAndServe()); err != nil {
+			panic(fmt.Sprintf("could not start Listener: %v", err))
+		}
+	}()
+
+	return nil
+}
+
+// Stop tries to stop the Listener and returns an error
+// if something went wrong. Ignores errors that were passed
+// by options and if used skip errors.
+func (l *listener) Stop() error {
+	if l.server == nil {
+		return l.catch(ErrEmptyListener)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), l.shutdownTimeout)
+	defer cancel()
+
+	ch := make(chan error, 1)
+
+	go func() {
+		ch <- l.catch(l.server.Shutdown(ctx))
+	}()
+
+	select {
+	case <-ctx.Done():
+		return nil
+	case err := <-ch:
+		return err
+	}
+}
+
+func (l *listener) catch(err error) error {
+	if l.skipErrors {
+		return nil
+	}
+
+	for i := range l.ignoreErrors {
+		if l.ignoreErrors[i] == err {
+			return nil
+		}
+	}
+
+	return err
+}
