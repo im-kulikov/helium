b6e83e7db3a26f658ac697eaf1580e053fce7334
Evgeniy Kulikov <im@kulikov.im>
2020-03-10 15:37:15 +0300

    add Defaults handler

--- settings/app.go
+++ settings/app.go
@@ -1,16 +1,25 @@
 package settings
 
-import "github.com/im-kulikov/helium/module"
+import (
+	"github.com/im-kulikov/helium/module"
+	"go.uber.org/dig"
+)
 
-// Core configuration
-type Core struct {
-	File         string
-	Type         string
-	Name         string
-	Prefix       string
-	BuildTime    string
-	BuildVersion string
-}
+type (
+	// Defaults is callback that allows to setup application before run.
+	// Helium calls `defaults` handler at the end in `New` method.
+	Defaults func(di *dig.Container) error
+
+	// Core configuration
+	Core struct {
+		File         string
+		Type         string
+		Name         string
+		Prefix       string
+		BuildTime    string
+		BuildVersion string
+	}
+)
 
 // Provider - wrap app config to provider
 func (a *Core) Provider() *module.Provider {
